14663f2c602f9d810a4374ca1fd2eb8ebafa57cf
Fountain <42763575+fountain-github@users.noreply.github.com>
2018-12-30 16:58:22 +0800

    Upgrade to Fountain 2 (#8)
    * Update FTN-Genesis-Contract.sol
    
    * Update README.md

--- SRC/FTN-Genesis-Contract.sol
+++ SRC/FTN-Genesis-Contract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity 0.4.25;
 
 library SafeMath {
 	function mul (uint256 a, uint256 b) internal pure returns (uint256) {
@@ -136,6 +136,10 @@ contract TokenForge is Ownable {
 	}
 }
 
+
+
+
+
 contract CappedToken is Ownable {
 	using SafeMath for uint256;
 
@@ -253,7 +257,7 @@ contract MintAndBurnToken is TokenForge, CappedToken, LockableProtocol {
 	}
 
 	function mint (address target, uint256 amount) public hasMintability whenRunning canForge returns (bool) {
-		require(target != owner && target != foundationOwner); // Owner和FoundationOwner不能成为mint的对象
+		require(target != owner && target != foundationOwner);
 		require(canMint(amount));
 
 		if (msg.sender == foundationOwner) {
@@ -274,7 +278,7 @@ contract MintAndBurnToken is TokenForge, CappedToken, LockableProtocol {
 		require(amount <= balance);
 
 		token_created = token_created.sub(amount);
-		wallets[msg.sender] -= amount;
+		wallets[msg.sender] = wallets[msg.sender].sub(amount);
 
 		emit Burn(msg.sender, amount);
 		emit Transfer(msg.sender, address(0), amount);
@@ -328,6 +332,7 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 	function setInvest (uint256 release_start, uint256 release_duration) public onlyOwner whenRunning returns (bool) {
 		releaseStart = release_start;
 		releaseDuration = release_duration;
+		require(releaseStart + releaseDuration > releaseStart);
 		forceStopInvest = false;
 
 		emit NewInvest(release_start, release_duration);
@@ -412,9 +417,9 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 		address r;
 		for (i = 0; i < investorsLength; i ++) {
 			r = investors[i];
-			if (r == address(0) || r == owner) continue;
+			if (r == address(0) || r == owner || r == foundationOwner) continue;
 			investorsCount ++;
-			totalAmount += amounts[i];
+			totalAmount = totalAmount.add(amounts[i]);
 		}
 		require(totalAmount > 0);
 		require(canMint(totalAmount));
@@ -423,8 +428,9 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 		token_created = token_created.add(totalAmount);
 		for (i = 0; i < investorsLength; i ++) {
 			r = investors[i];
-			if (r == address(0) || r == owner) continue;
+			if (r == address(0) || r == owner || r == foundationOwner) continue;
 			amount = amounts[i];
+			if (amount == 0) continue;
 			wallets[r] = wallets[r].add(amount);
 			emit Mint(r, amount);
 			emit Transfer(address(0), r, amount);
@@ -510,7 +516,7 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 		uint256 totalAmount = amount.mul(uint256(receiverCount));
 		require(canPay(msg.sender, totalAmount));
 
-		wallets[msg.sender] -= totalAmount;
+		wallets[msg.sender] = wallets[msg.sender].sub(totalAmount);
 		for (i = 0; i < receiveLength; i++) {
 			r = receivers[i];
 			if (r == address(0) || r == owner) continue;
@@ -532,12 +538,12 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 			r = receivers[i];
 			if (r == address(0) || r == owner) continue;
 			receiverCount ++;
-			totalAmount += amounts[i];
+			totalAmount = totalAmount.add(amounts[i]);
 		}
 		require(totalAmount > 0);
 		require(canPay(msg.sender, totalAmount));
 
-		wallets[msg.sender] -= totalAmount;
+		wallets[msg.sender] = wallets[msg.sender].sub(totalAmount);
 		uint256 amount;
 		for (i = 0; i < receiveLength; i++) {
 			r = receivers[i];
@@ -593,7 +599,7 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 			warrants[from][msg.sender] = warrant.sub(totalAmount);
 		}
 
-		wallets[from] -= totalAmount;
+		wallets[from] = wallets[from].sub(totalAmount);
 		for (i = 0; i < receiveLength; i++) {
 			r = receivers[i];
 			if (r == address(0) || r == owner) continue;
@@ -617,7 +623,7 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 			r = receivers[i];
 			if (r == address(0) || r == owner) continue;
 			receiverCount ++;
-			totalAmount += amounts[i];
+			totalAmount = totalAmount.add(amounts[i]);
 		}
 		require(totalAmount > 0);
 		require(canPay(from, totalAmount));
@@ -629,7 +635,7 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 			warrants[from][msg.sender] = warrant.sub(totalAmount);
 		}
 
-		wallets[from] -= totalAmount;
+		wallets[from] = wallets[from].sub(totalAmount);
 		uint256 amount;
 		for (i = 0; i < receiveLength; i++) {
 			r = receivers[i];
@@ -644,7 +650,7 @@ contract LockableToken is MintAndBurnToken, DelegatableToken {
 }
 
 contract FountainToken is LockableToken {
-	string  public constant name     = "Fountain";
+	string  public constant name     = "Fountain 2";
 	string  public constant symbol   = "FTN";
 	uint8   public constant decimals = 18;
 
@@ -692,4 +698,199 @@ contract FountainToken is LockableToken {
 		return true;
 	}
 	
+}
+
+
+
+contract FountainTokenUpgrade is FountainToken {
+	event UpgradeStart();
+	event UpgradeStop();
+	event SetRefund(address, uint);
+	event Refund(address, uint);
+	event SetFoundation(uint);
+	event FinishUpgrade();
+	
+	bool public upgrade_running;
+
+	bool public upgrade_finish;
+	
+	FountainToken ftn;
+	
+	address public oldContract;
+	
+	mapping(address=>bool) public upgraded;
+	mapping(address=>bool) public skiplist;
+
+	mapping(address=>uint) public refundlist;
+
+
+	constructor(address old){
+		oldContract = old;
+		ftn = FountainToken(old);
+	}
+
+	modifier canUpgrade(){
+		require(!upgrade_finish);
+		_;
+	}
+
+	modifier whenUpgrading() {
+		require(upgrade_running);
+		_;
+	}
+
+	modifier whenNotUpgrading() {
+		require(!upgrade_running);
+		_;
+	}
+
+	function finishUpgrade() public whenNotUpgrading canUpgrade onlyOwner{
+		upgrade_finish = true;
+		emit FinishUpgrade();
+	}
+
+	function setFoundation(uint amount) public whenUpgrading whenPaused canUpgrade onlyOwner {
+		token_foundation_created = amount;
+		emit SetFoundation(amount);
+	}
+
+	function setRefund(address addr, uint amount) public whenUpgrading canUpgrade onlyOwner {
+		require(addr != address(0));
+		require(addr != foundationOwner);
+		require(addr != owner);
+		refundlist[addr] = amount;
+		emit SetRefund(addr, amount);
+	}
+
+	function batchSetRefund(address[] addrs, uint[] amounts) public whenUpgrading canUpgrade onlyOwner {
+		uint l1 = addrs.length;
+		uint l2 = amounts.length;
+		address addr;
+		uint amount;
+		require(l1 > 0 && l1 == l2);
+		for (uint i = 0; i < l1; i++){
+			addr = addrs[i];
+			amount = amounts[i];
+			if (addr == address(0) || addr == foundationOwner || addr == owner) continue;
+			refundlist[addr] = amount;
+			emit SetRefund(addr, amount);
+		}
+	}
+
+
+	function runRefund(address addr) public whenUpgrading canUpgrade onlyOwner {
+		uint amount = refundlist[addr];
+		wallets[addr] = wallets[addr].add(amount); 
+		token_created = token_created.add(amount);
+		refundlist[addr] = 0;
+		emit Refund(addr, amount);
+		emit Mint(addr, amount);
+		emit Transfer(address(0), addr, amount);
+	}
+
+	function batchRunRefund(address[] addrs) public whenUpgrading canUpgrade onlyOwner {
+		uint l = addrs.length;
+		address addr;
+		uint amount;
+		require(l > 0);
+		for (uint i = 0; i < l; i++){
+			addr = addrs[i];
+			amount = refundlist[addr];
+			wallets[addr] = wallets[addr].add(amount); 
+			token_created = token_created.add(amount);
+			refundlist[addr] = 0;
+			emit Refund(addr, amount);
+			emit Mint(addr, amount);
+			emit Transfer(address(0), addr, amount);
+		}
+	}
+
+	function startUpgrade() public whenNotUpgrading canUpgrade onlyOwner {
+		upgrade_running = true;
+		emit UpgradeStart();
+	}
+
+	function stopUpgrade() public whenUpgrading canUpgrade onlyOwner {
+		upgrade_running = false;
+		emit UpgradeStop();
+	}
+
+	function setSkiplist(address[] addrs) public whenUpgrading whenPaused canUpgrade onlyOwner {
+		uint len = addrs.length;
+		if (len>0){
+			for (uint i = 0; i < len; i++){
+				skiplist[addrs[i]] = true;
+			}
+		}
+	}
+
+	function upgrade(address addr) whenUpgrading whenPaused canUpgrade onlyOwner{
+		uint amount = ftn.balanceOf(addr);
+		require(!upgraded[addr] && amount>0 && !skiplist[addr]);
+
+		upgraded[addr] = true;
+		wallets[addr] = amount;
+
+		(uint a, uint b, uint c, uint d) = ftn.lockbins(addr,0);
+		uint len = d;
+		if (len > 0){
+			lockbins[addr][0].amount = len; 
+			for (uint i=1; i <= len; i++){
+				(a, b, c, d) = ftn.lockbins(addr,i);
+				lockbins[addr][i] = LockBin({
+					start: a,
+					finish: b,
+					duration: c,
+					amount: d
+				});
+			}
+		}
+
+		token_created = token_created.add(amount);
+		emit Mint(addr, amount);
+		emit Transfer(address(0), addr, amount);
+	}
+	
+	
+	function batchUpgrade(address[] addrs) whenUpgrading whenPaused canUpgrade onlyOwner{
+		uint l = addrs.length;
+		require(l > 0);
+		uint a;
+		uint b; 
+		uint c; 
+		uint d;
+		for (uint i = 0; i < l; i++){
+
+			address addr = addrs[i];
+			uint amount = ftn.balanceOf(addr);
+			if (upgraded[addr] || amount == 0 || skiplist[addr]){
+				continue;
+			}
+
+			upgraded[addr] = true;
+			wallets[addr] = amount;
+	
+			(a, b, c, d) = ftn.lockbins(addr,0);
+			uint len = d;
+			if (len > 0){
+				lockbins[addr][0].amount = len; 
+				for (uint j=1; j <= len; j++){
+					(a, b, c, d) = ftn.lockbins(addr, j);
+					lockbins[addr][j] = LockBin({
+						start: a,
+						finish: b,
+						duration: c,
+						amount: d
+					});
+				}
+			}
+
+			token_created = token_created.add(amount);
+			emit Mint(addr, amount);
+			emit Transfer(address(0), addr, amount);
+
+		} 
+		
+	}
+
 }
